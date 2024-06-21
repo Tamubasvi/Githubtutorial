8fa1d77a526b666e751af03b15a5cf38c7f5eadf
tamubasvi <tamubasvi@gmail.com>
2024-06-21 13:22:23 -0400

    Initial Commit

---
+++ KWK THING/KWK_THINGApp.swift
@@ -0,0 +1,17 @@
+//
+//  KWK_THINGApp.swift
+//  KWK THING
+//
+//  Created by Scholar on 6/21/24.
+//
+
+import SwiftUI
+
+@main
+struct KWK_THINGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
